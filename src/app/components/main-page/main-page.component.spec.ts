7a07d36e09e585d241752d4b1bab47c4770f3980
Vanderosky <kajtektokarski@gmail.com>
2021-04-27 21:53:13 +0200

    improvements to tests

--- src/app/components/main-page/main-page.component.spec.ts
+++ src/app/components/main-page/main-page.component.spec.ts
@@ -1,7 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
-
 import { MainPageComponent } from './main-page.component';
 
 describe('MainPageComponent', () => {
